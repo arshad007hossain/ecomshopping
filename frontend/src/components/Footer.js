771e65a3706a1d57c7029a6634c558d0bc24f34f
arshad007hossen <arshad417hossain@gmail.com>
2022-08-27 11:55:21 +0600

    added client id and fix header

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer>
       <Container>
         <Row>
-          <Col className='text-center py-3'>Copyright &copy; ProShop</Col>
+          <Col className='text-center py-3'>Copyright &copy; EShop</Col>
         </Row>
       </Container>
     </footer>
